aa5aaef009e2bebbde28edcaa552615439f908d7
Kanstantin99 <kostyaskoblikov66@gmail.com>
2023-04-13 00:56:02 +0300

    hw1Ready

---
+++ hm1/index.js
@@ -0,0 +1,35 @@
+var welcome = "Welcome to the homework 1";
+alert(welcome);
+console.log(welcome);
+
+var iHave = "У меня есть переменные вот такого типа :";
+alert(iHave);
+console.log(iHave);
+
+var typeString = "Тип первый - Строчный ";
+alert(typeString);
+console.log(typeString);
+console.log(typeof typeString);
+
+var typeNumber = 2222222;
+alert(typeNumber);
+console.log(typeNumber);
+console.log(typeof typeNumber);
+
+var typeBigAmount = 12345678901234567891234567890n;
+alert(typeBigAmount);
+console.log(typeBigAmount);
+console.log(typeof typeBigAmount);
+
+var typeBoolean = true;
+alert(typeBoolean);
+console.log(typeBoolean);
+console.log(typeof typeBoolean);
+
+var youLike = "Понравилось?";
+alert(youLike);
+console.log(youLike);
+
+var yes = "Да";
+alert(yes);
+console.log(yes);
