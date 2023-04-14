6f0b6d1e2d9274311e44a321e6a46f49f6ac5ee5
Kanstantin99 <kostyaskoblikov66@gmail.com>
2023-04-14 10:14:14 +0300

    hwfinal

--- hm1/index.js
+++ hm1/index.js
@@ -1,34 +1,27 @@
 var welcome = "Welcome to the homework 1";
 alert(welcome);
-console.log(welcome);
 
 var iHave = "У меня есть 4 переменные вот такого типа :";
 alert(iHave);
-console.log(iHave);
 
-alert("Строчный тип ");
-console.log("Строчный тип ");
-console.log(typeof "Строчный тип ");
+var string = "Строчный тип";
+alert(string);
+alert(typeof string);
 
 var typeNumber = 2222222;
 alert(typeNumber);
-console.log(typeNumber);
-console.log(typeof typeNumber);
+alert(typeof typeNumber);
 
 var typeBigAmount = 12345678901234567891234567890n;
 alert(typeBigAmount);
-console.log(typeBigAmount);
-console.log(typeof typeBigAmount);
+alert(typeof typeBigAmount);
 
 var typeBoolean = true;
 alert(typeBoolean);
-console.log(typeBoolean);
-console.log(typeof typeBoolean);
+alert(typeof typeBoolean);
 
 var youLike = "Понравилось ли вам нажимать на кнопки?";
-alert(youLike);
-console.log(youLike);
+confirm(youLike);
 
 var yes = "Да";
-confirm(yes);
-console.log(yes);
+alert(yes);
