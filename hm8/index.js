e2b7d8355f1f898f8f1c13f2506a4a432e0de1cb
Kanstantin99 <kostyaskoblikov66@gmail.com>
2023-05-23 20:32:46 +0300

    hw8

---
+++ hm8/index.js
@@ -0,0 +1,44 @@
+const colors = [
+  "#26543484",
+  "#04068984",
+  "#94087884",
+  "#64060684",
+  "#91950384",
+  "#19882284",
+  "#18799784",
+  "#52265484",
+  "#49030384",
+  "#09479884",
+];
+
+/*
+let blocks = document.querySelectorAll(".block");
+blocks.forEach((block, index) => {
+  block.style.backgroundColor = colors[index];
+});
+document.querySelector(".container").addEventListener("click", (event) => {
+  let currentColor = event.target.style.backgroundColor;
+
+  blocks.forEach((block) => {
+    block.style.backgroundColor = currentColor;
+  });
+});
+*/
+
+let blocks = document.querySelectorAll(".block");
+blocks.forEach((block, index) => {
+  block.style.backgroundColor = colors[index];
+});
+blocks.forEach((block) => {
+  block.addEventListener("click", function () {
+    let clickedColor = this.style.backgroundColor.replace("#", "");
+    for (let i = 0; i < colors.length; i++) {
+      let currentColor = colors[i].replace("#", "");
+      let newColor = clickedColor + currentColor;
+      colors[i] = newColor;
+      blocks[i].style.backgroundColor = newColor;
+    }
+  });
+
+  console.log(colors);
+});
