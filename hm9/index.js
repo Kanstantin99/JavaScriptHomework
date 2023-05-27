2d97d055b82df092c0006ca25e8e2132bb5ccb50
Kanstantin99 <kostyaskoblikov66@gmail.com>
2023-05-27 23:38:21 +0300

    chessdesk

--- hm9/index.js
+++ hm9/index.js
@@ -1,16 +1,17 @@
-const gorizont = document.getElementById("gorizont");
-const vertical = document.getElementById("vertical");
+let gorizont = document.getElementById("gorizont");
+let vertical = document.getElementById("vertical");
 const create = document.getElementById("create");
 const desk = document.getElementById("desk");
 const error = document.getElementById("error");
 
 create.onclick = () => {
+  gorizont = document.getElementById("gorizont").value;
+  vertical = document.getElementById("vertical").value;
   if (isNaN(gorizont) || isNaN(vertical)) {
     error.textContent = "Please type a number";
   } else {
     error.textContent = "";
     desk.innerHTML = "";
-
     for (let i = 0; i < gorizont; i++) {
       const row = document.createElement("div");
       row.className = "block";
@@ -27,11 +28,20 @@ create.onclick = () => {
     }
   }
 };
-
-desk.addEventListener("click", function (event) {
-  const cell = event.target.closest(".cell");
-  if (cell) {
-    cell.classList.toggle("white");
-    cell.classList.toggle("black");
+desk.addEventListener("click", function () {
+  const cells = document.querySelectorAll(".cell");
+  function toggleColors() {
+    cells.forEach((cell) => {
+      if (cell.classList.contains("white")) {
+        cell.classList.remove("white");
+        cell.classList.add("black");
+      } else if (cell.classList.contains("black")) {
+        cell.classList.remove("black");
+        cell.classList.add("white");
+      }
+    });
   }
+  cells.forEach((cell) => {
+    cell.addEventListener("click", toggleColors);
+  });
 });
